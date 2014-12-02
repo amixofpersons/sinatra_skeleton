b1b2656485129a920a9f1f3c105dd6601e35606f
maggie <myustinreyes@gmail.com>
2014-12-02 17:45:25 -0500

    users now have visible profiles

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,21 +1,24 @@
-#Read
 get '/login' do
   erb :'auth/login'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect('/')
 end
 
 get '/signup' do
   erb :'auth/signup'
 end
 
-get '/logout' do
-  session[:user] = nil
-  redirect('/')
+get '/user/:id' do
+  @user = User.find(params[:id])
+  erb :'auth/profile'
 end
 
 #Create
 post '/signup' do
   user = User.new(params[:user])
-
   if user.save
     session[:user_id] = user.id
     redirect('/')
@@ -26,7 +29,8 @@ post '/signup' do
 end
 
 post '/login' do
-  user = User.find_by(name: params[:name]).try(:authenticate, params[:password])
+  user = User.find_by(name: params[:name]).try(:authenticate,
+    params[:password])
 
   if user
     session[:user_id] = user.id
--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-game = Game.new()
-game.add_players("Maggie", "Patricia")
-game.save
